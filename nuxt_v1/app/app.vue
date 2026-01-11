e13e90188db05595b5a51abb4bdae7e0af745929
Mega-117 <enricosaian@hotmail.it>
2026-01-11 16:27:35 +0100

    Build Nuxt v1

---
+++ nuxt_v1/app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div style="padding:40px;font-family:sans-serif">
+    <h1>ACC Drive – Nuxt Test</h1>
+    <p>Se vedi questa pagina, GitHub Pages funziona.</p>
+  </div>
+</template>
---
+++ nuxt_v1/nuxt.config.ts
@@ -0,0 +1,12 @@
+export default defineNuxtConfig({
+  ssr: false,
+
+  app: {
+    baseURL: '/test_nuxt/',
+    buildAssetsDir: 'assets'
+  },
+
+  nitro: {
+    preset: 'static'
+  }
+})
